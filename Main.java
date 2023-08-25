5e14fc26e252bab58d58b4068341a67f219c8153
Сафаров Эдуард <edyard174@mail.ru>
2023-08-26 00:18:23 +0500

    lession_2

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -6;
+        int b = 5;
+        int i = a + b;
+        if (i >= 0) System.out.println("Сумма положительная");
+        else System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor() {
+        int value = 1;
+        if (value <= 0) System.out.println("Красный");
+        else if (value <= 100) System.out.println("Желтый");
+        else System.out.println("Зеленый");
+    }
+
+    public static void compareNumbers() {
+        int a = 3, b = 4;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else System.out.println("a < b");
     }
 }
