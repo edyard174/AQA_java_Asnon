38557e815d55170d9b4019837c0d4f89646285cd
Сафаров Эдуард <edyard174@mail.ru>
2023-10-04 02:14:39 +0500

    lession_12_junit_5

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial {
+
+    public long getFactorial(int f) {
+        if (f <= 1) {
+            return 1;
+        }
+        else {
+            return f * getFactorial(f - 1);
+        }
+    }
+}
